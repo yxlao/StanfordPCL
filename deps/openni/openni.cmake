621b5c2ed7535ff25b522607bbdde6a646eba2ac
Yixing Lao <yixing.lao@gmail.com>
2021-06-04 02:35:29 -0700

    build openni inside deps

--- deps/openni/openni.cmake
+++ deps/openni/openni.cmake
@@ -13,20 +13,17 @@ include(ExternalProject)
 ExternalProject_Add(
     ext_openni
     PREFIX openni
-    URL ${PROJECT_SOURCE_DIR}/deps/openni/openni
+    URL ${PROJECT_SOURCE_DIR}/openni/openni
     BUILD_IN_SOURCE ON
     CONFIGURE_COMMAND ""
     UPDATE_COMMAND ""
+    INSTALL_DIR ${DEPS_INSTALL_PREFIX}
     BUILD_COMMAND   cd Platform/Linux/Build && make -j6
     COMMAND         cd Platform/Linux/CreateRedist && ./RedistMaker
     INSTALL_COMMAND echo "OpenNI will be installed to" <INSTALL_DIR>
     COMMAND         rm -rf <INSTALL_DIR>/usr <INSTALL_DIR>/var
     COMMAND         mkdir -p <INSTALL_DIR>/usr/lib <INSTALL_DIR>/usr/bin <INSTALL_DIR>/var
     COMMAND         cd Platform/Linux/Redist/OpenNI-Bin-Dev-Linux-x64-v1.5.7.10 && ./install.sh -c <INSTALL_DIR>
+    COMMAND         eval mv <INSTALL_DIR>/usr/* <INSTALL_DIR>
+    COMMAND         rm -rf <INSTALL_DIR>/var <INSTALL_DIR>/usr
 )
-
-ExternalProject_Get_Property(ext_openni INSTALL_DIR)
-set(OPENNI_FOUND TRUE)
-set(OPENNI_INCLUDE_DIRS ${INSTALL_DIR}/usr/include/ni/)
-set(OPENNI_LIBRARIES ${INSTALL_DIR}/usr/lib/libOpenNI.so})
-set(OPENNI_DEFINITIONS "")
