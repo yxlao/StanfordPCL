ce56455c13e1064724b94b058819ea0c8a0cd375
Yixing Lao <yixing.lao@gmail.com>
2021-06-03 22:17:27 -0700

    installs openni, add deps target

--- deps/openni/openni.cmake
+++ deps/openni/openni.cmake
@@ -17,12 +17,17 @@ ExternalProject_Add(
     BUILD_IN_SOURCE ON
     CONFIGURE_COMMAND ""
     UPDATE_COMMAND ""
-    BUILD_COMMAND cd Platform/Linux/Build && make
-    INSTALL_COMMAND ""
+    BUILD_COMMAND   cd Platform/Linux/Build && make -j6
+    COMMAND         cd Platform/Linux/CreateRedist && ./RedistMaker
+    INSTALL_COMMAND echo "OpenNI will be installed to" <INSTALL_DIR>
+    COMMAND         rm -rf <INSTALL_DIR>/usr <INSTALL_DIR>/var
+    COMMAND         mkdir -p <INSTALL_DIR>/usr/lib <INSTALL_DIR>/usr/bin <INSTALL_DIR>/var
+    COMMAND         cd Platform/Linux/Redist/OpenNI-Bin-Dev-Linux-x64-v1.5.7.10 && ./install.sh -c <INSTALL_DIR>
 )
 add_dependencies(ext_openni always_rebuild)
 
 ExternalProject_Get_Property(ext_openni INSTALL_DIR)
 set(OPENNI_FOUND TRUE)
-set(OPENNI_INCLUDE_DIRS ${INSTALL_DIR}) # "/" is critical.
-set(OPENNI_LIBRARIES ${INSTALL_DIR}})
+set(OPENNI_INCLUDE_DIRS ${INSTALL_DIR}/usr/include/ni/)
+set(OPENNI_LIBRARIES ${INSTALL_DIR}/usr/lib/libOpenNI.so})
+set(OPENNI_DEFINITIONS "")
