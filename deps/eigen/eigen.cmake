4756c8144492a5814691b2f9b1618bb401e7be67
Yixing Lao <yixing.lao@gmail.com>
2021-06-06 04:20:14 -0700

    use downloaded eigen

--- deps/eigen/eigen.cmake
+++ deps/eigen/eigen.cmake
@@ -3,7 +3,7 @@ include(ExternalProject)
 ExternalProject_Add(
     ext_eigen
     PREFIX eigen
-    URL ${PROJECT_SOURCE_DIR}/eigen/eigen
+    URL https://gitlab.com/libeigen/eigen/-/archive/3.0.4/eigen-3.0.4.zip
     INSTALL_DIR ${DEPS_INSTALL_PREFIX}
     CMAKE_ARGS
         -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
