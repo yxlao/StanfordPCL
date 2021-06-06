ce021e963652d227bbb1dd1d0906dc243bce1262
Yixing Lao <yixing.lao@gmail.com>
2021-06-06 05:42:17 -0700

    fix ABI issues

--- deps/opencv/opencv.cmake
+++ deps/opencv/opencv.cmake
@@ -6,8 +6,9 @@ ExternalProject_Add(
     URL https://github.com/opencv/opencv/archive/refs/tags/2.3.1.tar.gz
     INSTALL_DIR ${DEPS_INSTALL_PREFIX}
     CMAKE_ARGS
-        -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-        -DCMAKE_CXX_FLAGS=-Wno-narrowing
-        -DWITH_FFMPEG=OFF
-        -DWITH_CUDA=OFF
+        "-DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>"
+        "-DCMAKE_CXX_FLAGS=-Wno-narrowing -D_GLIBCXX_USE_CXX11_ABI=0"
+        "-DWITH_FFMPEG=OFF"
+        "-DWITH_CUDA=OFF"
+        "-DBUILD_TESTS=OFF"
 )
