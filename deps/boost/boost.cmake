2d7af536befaa45d17e4a45f8d65fa8a7a0b3cfb
Yixing Lao <yixing.lao@gmail.com>
2021-06-06 04:59:07 -0700

    Revert "use downloaded boost"
    This reverts commit b251e21c8ed5659079dec37f5749c6bb22ec8b92.

--- deps/boost/boost.cmake
+++ deps/boost/boost.cmake
@@ -1,19 +1,3 @@
-# include(ExternalProject)
-# include(ProcessorCount)
-# ProcessorCount(NPROC)
-
-# ExternalProject_Add(
-#     ext_boost
-#     PREFIX boost
-#     URL ${PROJECT_SOURCE_DIR}/boost/boost_1_50_0.tar.bz2
-#     INSTALL_DIR ${DEPS_INSTALL_PREFIX}
-#     BUILD_IN_SOURCE ON
-#     UPDATE_COMMAND ""
-#     CONFIGURE_COMMAND ./bootstrap.sh -prefix=<INSTALL_DIR>
-#     BUILD_COMMAND     ./b2 toolset=gcc-4.8 -j ${NPROC}
-#     INSTALL_COMMAND   ./b2 toolset=gcc-4.8 install -j ${NPROC}
-# )
-
 include(ExternalProject)
 include(ProcessorCount)
 ProcessorCount(NPROC)
@@ -21,18 +5,11 @@ ProcessorCount(NPROC)
 ExternalProject_Add(
     ext_boost
     PREFIX boost
-    DOWNLOAD_COMMAND wget https://github.com/boostorg/boost/archive/refs/tags/boost-1.50.0.zip # boost-boost
-    COMMAND          unzip boost-1.50.0.zip && rm -rf boost-1.50.0.zip
-    COMMAND          wget https://github.com/boostorg/build/archive/refs/tags/boost-1.50.0.zip # boost-build
-    COMMAND          unzip boost-1.50.0.zip && rm -rf boost-1.50.0.zip
-    COMMAND          rm -rf ext_boost && mkdir ext_boost
-    COMMAND          eval mv boost-boost-1.50.0/* ext_boost
-    COMMAND          eval mv build-boost-1.50.0/v2 ext_boost/tools/build
-    COMMAND          rm -rf boost-boost-1.50.0/ build-boost-1.50.0/
+    URL ${PROJECT_SOURCE_DIR}/boost/boost_1_50_0.tar.bz2
     INSTALL_DIR ${DEPS_INSTALL_PREFIX}
     BUILD_IN_SOURCE ON
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ./bootstrap.sh -prefix=<INSTALL_DIR>
-    BUILD_COMMAND ./b2 toolset=gcc-4.8 -j ${NPROC}
-    INSTALL_COMMAND ./b2 toolset=gcc-4.8 install -j ${NPROC}
+    BUILD_COMMAND     ./b2 toolset=gcc-4.8 -j ${NPROC}
+    INSTALL_COMMAND   ./b2 toolset=gcc-4.8 install -j ${NPROC}
 )
