94791118c872dda1b6fbf8cac1b616667db9207f
Yixing Lao <yixing.lao@gmail.com>
2021-06-06 20:32:38 -0700

    flann use git and patch

--- deps/flann/flann.cmake
+++ deps/flann/flann.cmake
@@ -3,8 +3,11 @@ include(ExternalProject)
 ExternalProject_Add(
     ext_flann
     PREFIX flann
-    URL ${PROJECT_SOURCE_DIR}/flann/flann
+    GIT_REPOSITORY https://github.com/flann-lib/flann.git
+    GIT_TAG 1.8.0-src
     INSTALL_DIR ${DEPS_INSTALL_PREFIX}
+    UPDATE_COMMAND ""
+    PATCH_COMMAND git apply ${PROJECT_SOURCE_DIR}/flann/0001-add-empty.cpp-and-upate-cmake.patch
     CMAKE_ARGS
         -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
         -DBUILD_MATLAB_BINDINGS=OFF
