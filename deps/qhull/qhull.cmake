9d8c16a650ad4819e1fdff3edeee6c2f7d7d6448
Yixing Lao <yixing.lao@gmail.com>
2021-06-07 02:28:14 -0700

    migrate dependencies

---
+++ deps/qhull/qhull.cmake
@@ -0,0 +1,10 @@
+include(ExternalProject)
+
+ExternalProject_Add(
+    ext_qhull
+    PREFIX qhull
+    URL https://github.com/qhull/qhull/archive/refs/tags/2011.2.zip
+    INSTALL_DIR ${PCL_DEPS_INSTALL_PREFIX}
+    CMAKE_ARGS
+        "-DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>"
+)
